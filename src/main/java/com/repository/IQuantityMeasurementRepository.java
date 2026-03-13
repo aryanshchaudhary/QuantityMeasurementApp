8acfe74a1ba2399b807b4327724e00df3542a91a
Aryansh Chaudhary <abhichaudhary012002@gmail.com>
2026-03-13 10:39:19 +0900

    [Aryansh Chaudhary] add . N-Tier Architecture Refactoring

---
+++ src/main/java/com/repository/IQuantityMeasurementRepository.java
@@ -0,0 +1,11 @@
+package com.repository;
+
+import com.entity.QuantityMeasurementEntity;
+import java.util.List;
+
+public interface IQuantityMeasurementRepository {
+
+    void save(QuantityMeasurementEntity entity);
+
+    List<QuantityMeasurementEntity> findAll();
+}
